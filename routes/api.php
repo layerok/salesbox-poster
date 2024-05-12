58cf8670c60e53d711cc7c9cef488a29a10b6263
Vladimir Rudomanenko <rudomanenkovladimir@gmail.com>
2024-05-12 19:35:16 +0300

    add an endpoint error logging

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/log-error', function() {
+    Log::error(Request::getContent());
 });
